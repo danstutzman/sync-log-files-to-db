08c6b8b878b3def6ffb53c38636732cb752f7a24
Dan Stutzman <dan@danstutzman.com>
2017-04-16 17:11:56 -0600

    Copy sample image into source bucket

--- deployer-go/deploy.go
+++ deployer-go/deploy.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"io/ioutil"
 	"log"
 )
 
@@ -27,6 +29,35 @@ func createBucket(s3Service *s3.S3, bucketName string) chan CreateBucketReturn {
 	return future
 }
 
+type CopyToBucketReturn struct{}
+
+func copyToBucket(s3Service *s3.S3, fromPath string, toBucketName string,
+	toPath string) chan CopyToBucketReturn {
+	future := make(chan CopyToBucketReturn)
+	go func() {
+		log.Printf("Copying %s to bucket %s...", fromPath, toBucketName)
+
+		fromBytes, err := ioutil.ReadFile(fromPath)
+		if err != nil {
+			log.Fatalf("Error from ReadFile: %s", err)
+		}
+
+		output, err := s3Service.PutObject(&s3.PutObjectInput{
+			Bucket: aws.String(toBucketName),
+			Key:    aws.String(toPath),
+			Body:   bytes.NewReader(fromBytes),
+		})
+		if err != nil {
+			log.Fatalf("Error from CreateBucket: %s", err)
+		}
+
+		log.Println(output)
+
+		future <- CopyToBucketReturn{}
+	}()
+	return future
+}
+
 func main() {
 	sourceBucketName := "danstutzman-lambda-example"
 	targetBucketName := sourceBucketName + "resized"
@@ -39,8 +70,13 @@ func main() {
 
 	s3Service := s3.New(awsSession)
 
-	createBucketFuture1 := createBucket(s3Service, sourceBucketName)
-	createBucketFuture2 := createBucket(s3Service, targetBucketName)
-	<-createBucketFuture1
-	<-createBucketFuture2
+	createSourceBucketFuture := createBucket(s3Service, sourceBucketName)
+	createTargetBucketFuture := createBucket(s3Service, targetBucketName)
+
+	<-createSourceBucketFuture
+	copySampleImageFuture := copyToBucket(s3Service, "../HappyFace.jpg",
+		sourceBucketName, "/HappyFace.jpg")
+
+	<-createTargetBucketFuture
+	<-copySampleImageFuture
 }
