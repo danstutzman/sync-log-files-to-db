ca5fc57e89095319cdd4b08a17d37a1aff3e3198
Dan Stutzman <dan@danstutzman.com>
2017-04-16 15:55:06 -0600

    Start with making two buckets in parallel

---
+++ deployer-go/deploy.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"log"
+)
+
+func createBucket(s3Service *s3.S3, bucketName string) chan struct{} {
+	future := make(chan struct{})
+	go func() {
+		log.Printf("Creating bucket %s...", bucketName)
+		output, err := s3Service.CreateBucket(&s3.CreateBucketInput{
+			Bucket: aws.String(bucketName),
+		})
+		if err != nil {
+			log.Fatalf("Error from CreateBucket: %s", err)
+		}
+
+		log.Println(output)
+
+		future <- struct{}{}
+	}()
+	return future
+}
+
+func main() {
+	sourceBucketName := "danstutzman-lambda-example"
+	targetBucketName := sourceBucketName + "resized"
+
+	// Initialize a session that the SDK will use to load configuration,
+	// credentials, and region from the shared config file. (~/.aws/config).
+	awsSession := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	s3Service := s3.New(awsSession)
+
+	createBucketFuture1 := createBucket(s3Service, sourceBucketName)
+	createBucketFuture2 := createBucket(s3Service, targetBucketName)
+	<-createBucketFuture1
+	<-createBucketFuture2
+}
