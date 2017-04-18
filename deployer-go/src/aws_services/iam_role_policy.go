f7ffff76bc80239b3edc7197f9ff953afe578f0b
Dan Stutzman <dan@danstutzman.com>
2017-04-17 20:22:51 -0600

    Add iam_role_policy.go

---
+++ deployer-go/src/aws_services/iam_role_policy.go
@@ -0,0 +1,55 @@
+package aws_services
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/iam"
+	"log"
+)
+
+type PutRolePolicyReturn struct{}
+
+func (awsServices *AwsServices) PutRolePolicy(roleName string, sourceBucket string, targetBucket string) chan PutRolePolicyReturn {
+	future := make(chan PutRolePolicyReturn)
+	go func() {
+		log.Printf("Put role policy on %s...", roleName)
+
+		output, err := awsServices.iamService.PutRolePolicy(&iam.PutRolePolicyInput{
+			RoleName:   aws.String(roleName),
+			PolicyName: aws.String(roleName + "-policy"),
+			PolicyDocument: aws.String(`{
+				"Version": "2012-10-17",
+				"Statement": [
+					{
+				 		"Effect": "Allow",
+				 		"Action": [
+				 			 "logs:*"
+				 		],
+				 		"Resource": "arn:aws:logs:*:*:*"
+					},
+					{
+				 		"Effect": "Allow",
+				 		"Action": [
+				 			 "s3:GetObject"
+				 		],
+				 		"Resource": "arn:aws:s3:::` + sourceBucket + `/*"
+					},
+					{
+				 		"Effect": "Allow",
+				 		"Action": [
+				 			 "s3:PutObject"
+				 		],
+				 		"Resource": "arn:aws:s3:::` + targetBucket + `/*"
+					}
+				]
+			}`),
+		})
+		if err != nil {
+			log.Fatalf("Error from PutRolePolicy: %s", err)
+		} else {
+			log.Printf("Output from PutRolePolicy: %s", output)
+		}
+
+		future <- PutRolePolicyReturn{}
+	}()
+	return future
+}
