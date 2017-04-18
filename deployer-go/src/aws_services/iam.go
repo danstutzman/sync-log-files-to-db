7a86b223e1d409aea3e103883528ee7f1cdbd38f
Dan Stutzman <dan@danstutzman.com>
2017-04-17 19:58:17 -0600

    Refactor to split out iam.go

---
+++ deployer-go/src/aws_services/iam.go
@@ -0,0 +1,80 @@
+package aws_services
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/iam"
+	"log"
+)
+
+type CreateRoleNonIdempotentReturn struct{}
+
+func createRoleNonIdempotent(iamService *iam.IAM,
+	roleName string) chan CreateRoleNonIdempotentReturn {
+
+	future := make(chan CreateRoleNonIdempotentReturn)
+	go func() {
+		log.Printf("Creating role named %s...", roleName)
+
+		output, err := iamService.CreateRole(&iam.CreateRoleInput{
+			AssumeRolePolicyDocument: aws.String(`{
+				"Version": "2012-10-17",
+				"Statement": [
+					{
+						"Sid": "",
+						"Effect": "Allow",
+						"Principal": {
+							"Service": "lambda.amazonaws.com"
+						},
+						"Action": "sts:AssumeRole"
+					}
+				]
+			}`),
+			Path:     aws.String("/"),
+			RoleName: aws.String(roleName),
+		})
+		if err != nil {
+			log.Fatalf("Error from CreateRole: %s", err)
+		} else {
+			log.Printf("Output from CreateRole: %s", output)
+		}
+
+		future <- CreateRoleNonIdempotentReturn{}
+	}()
+	return future
+}
+
+func getArnForRole(iamService *iam.IAM, roleName string) (string, error) {
+	output, err := iamService.GetRole(&iam.GetRoleInput{
+		RoleName: aws.String(roleName),
+	})
+	if err != nil {
+		return "", err
+	} else {
+		log.Printf("Output from GetRole: %s", output)
+		return *output.Role.Arn, err
+	}
+}
+
+type CreateRoleIdempotentReturn struct {
+	arn string
+}
+
+func (awsServices *AwsServices) CreateRoleIdempotent(roleName string) chan CreateRoleIdempotentReturn {
+	future := make(chan CreateRoleIdempotentReturn)
+	go func() {
+		arn, err := getArnForRole(awsServices.iamService, roleName)
+		if err != nil {
+			if err.Error() == iam.ErrCodeNoSuchEntityException {
+				<-createRoleNonIdempotent(awsServices.iamService, roleName)
+				arn, err = getArnForRole(awsServices.iamService, roleName)
+				if err != nil {
+					log.Fatalf("Error from second GetRole: %s", err)
+				}
+			} else {
+				log.Fatalf("Error from first GetRole: %s", err)
+			}
+		}
+		future <- CreateRoleIdempotentReturn{arn: arn}
+	}()
+	return future
+}
