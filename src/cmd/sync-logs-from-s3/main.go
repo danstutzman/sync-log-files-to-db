48143dcf21ba1044ed2441583cb089aaa4659ef9
Daniel Stutzman <dtstutz@gmail.com>
2017-11-08 14:50:43 -0700

    Combined config files bigquery.json and s3.json

--- src/cmd/sync-logs-from-s3/main.go
+++ src/cmd/sync-logs-from-s3/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/danielstutzman/sync-logs-from-s3/src/storage/bigquery"
 	my_s3 "github.com/danielstutzman/sync-logs-from-s3/src/storage/s3"
@@ -11,24 +12,31 @@ import (
 
 const NUM_PATHS_PER_PAGE = 1000
 
-func main() {
-	bigqueryOptionsBytes, err := ioutil.ReadFile("config/bigquery.json")
-	if err != nil {
-		log.Fatalf("Error from ReadFile: %s", err)
-	}
-	var bigqueryOptions bigquery.Options
-	json.Unmarshal(bigqueryOptionsBytes, &bigqueryOptions)
-	bigquery.ValidateOptions(&bigqueryOptions)
-	bigqueryConn := bigquery.NewBigqueryConnection(&bigqueryOptions)
+type Config struct {
+	Bigquery bigquery.Options
+	S3       my_s3.Options
+}
 
-	s3OptionsBytes, err := ioutil.ReadFile("config/s3.json")
-	if err != nil {
-		log.Fatalf("Error from ReadFile: %s", err)
+func main() {
+	if len(os.Args) < 1+1 {
+		log.Fatalf("First argument should be config.json")
 	}
-	var s3Options my_s3.Options
-	json.Unmarshal(s3OptionsBytes, &s3Options)
-	my_s3.ValidateOptions(&s3Options)
-	s3Connection := my_s3.NewS3Connection(&s3Options)
+
+	var config = &Config{}
+	configJson, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatalf("Error from ioutil.ReadFile: %s", err)
+	}
+	err = json.Unmarshal(configJson, &config)
+	if err != nil {
+		log.Fatalf("Error from json.Unmarshal: %s", err)
+	}
+
+	bigquery.ValidateOptions(&config.Bigquery)
+	bigqueryConn := bigquery.NewBigqueryConnection(&config.Bigquery)
+
+	my_s3.ValidateOptions(&config.S3)
+	s3Connection := my_s3.NewS3Connection(&config.S3)
 
 	visits := []map[string]string{}
 	pageOfPaths := s3Connection.ListPaths(NUM_PATHS_PER_PAGE)
