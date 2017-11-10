506a3669517b5c71f759df216d719ae40d80b23c
Daniel Stutzman <dtstutz@gmail.com>
2017-11-10 12:14:32 -0700

    Be robust to truncated log lines.

--- src/sources/docker/log_line.go
+++ src/sources/docker/log_line.go
@@ -88,7 +88,8 @@ func readLogLineBlocking(out io.Reader) *LogLine {
 	// Read 8-byte header (blocking)
 	// See https://docs.docker.com/engine/api/v1.24/ for header format
 	header := make([]byte, 8)
-	numBytes, err := out.Read(header)
+	numBytes, err := io.ReadFull(out, header)
+	//numBytes, err := out.Read(header)
 	if numBytes == 0 {
 		return nil
 	}
@@ -105,9 +106,10 @@ func readLogLineBlocking(out io.Reader) *LogLine {
 
 	// Read line (blocking)
 	line := make([]byte, lineLen)
-	numBytes, err = out.Read(line)
+	//numBytes, err = out.Read(line)
+	numBytes, err = io.ReadFull(out, line)
 	if uint32(numBytes) < lineLen {
-		log.Fatalf("Expected %d but got %d", lineLen, numBytes)
+		line = append(line[0:numBytes], []byte("(truncated)")...)
 	}
 	if err != nil {
 		log.Fatalf("Error from Read: %s", err)
@@ -115,15 +117,20 @@ func readLogLineBlocking(out io.Reader) *LogLine {
 
 	// Parse timestamp in line
 	match := DOCKER_LOG_LINE_REGEXP.FindSubmatch(line)
-	timestamp, err := time.Parse(DOCKER_LOG_TIME_FORMAT, string(match[1]))
-	message := string(match[2])
-	if err != nil {
-		log.Fatalf("Can't parse timestamp %s", string(match[1]))
-	}
+	if len(match) == 0 {
+		log.Printf("No DOCKER_LOG_LINE_REGEXP match for line: %s", line)
+		return nil
+	} else {
+		timestamp, err := time.Parse(DOCKER_LOG_TIME_FORMAT, string(match[1]))
+		message := string(match[2])
+		if err != nil {
+			log.Fatalf("Can't parse timestamp %s", string(match[1]))
+		}
 
-	return &LogLine{
-		StreamType: streamType,
-		Timestamp:  timestamp,
-		Message:    message,
+		return &LogLine{
+			StreamType: streamType,
+			Timestamp:  timestamp,
+			Message:    message,
+		}
 	}
 }
