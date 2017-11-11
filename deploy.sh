b8e2a20d57e88b715e2855a215a704fd00f72a09
Daniel Stutzman <dtstutz@gmail.com>
2017-11-10 17:23:44 -0700

    Deploy grafana docker app

--- deploy.sh
+++ deploy.sh
@@ -45,4 +45,15 @@ lsof -i :6380 || docker run -d \
   --restart unless-stopped \
   sync-log-files-to-db /root/sync-log-files-to-db /root/config/config.json.prod
 
+mkdir -p /root/grafana/data
+# If nothing is listening on port 3000, start grafana on port 3000.
+lsof -i :3000 || docker run -d \
+  --name=grafana \
+  -p 3000:3000 \
+  -v /root/grafana/data:/var/lib/grafana \
+  -v /root/grafana/config:/etc/grafana \
+  --restart unless-stopped \
+  -e GF_INSTALL_PLUGINS=belugacdn-app \
+  grafana/grafana
+
 EOF
